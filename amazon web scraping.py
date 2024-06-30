2d932ae82aca4b623b6e72fd8fc67c97879cb5de
Akhand Agrawal <83289867+AkhandAgrawal@users.noreply.github.com>
2024-07-01 00:46:40 +0530

    Update amazon web scraping.py

--- amazon web scraping.py
+++ amazon web scraping.py
@@ -3,17 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import numpy as np
-# Function to extract Product Title
+
 def get_title(soup):
 
     try:
-        # Outer Tag Object
+        
         title = soup.find("span", attrs={"id":'productTitle'})
         
-        # Inner NavigatableString Object
         title_value = title.text
 
-        # Title as a string value
         title_string = title_value.strip()
 
     except AttributeError:
@@ -21,7 +19,7 @@ def get_title(soup):
 
     return title_string
 
-# Function to extract Product Price
+
 def get_price(soup):
 
     try:
@@ -38,7 +36,6 @@ def get_price(soup):
 
     return price
 
-# Function to extract Product Rating
 def get_rating(soup):
 
     try:
@@ -62,7 +59,6 @@ def get_review_count(soup):
 
     return review_count
 
-# Function to extract Availability Status
 def get_availability(soup):
     try:
         available = soup.find("div", attrs={'id':'availability'})
@@ -74,37 +70,31 @@ def get_availability(soup):
     return available
 if __name__ == '__main__':
 
-    # add your user agent 
+
     HEADERS = ({'User-Agent':'', 'Accept-Language': 'en-US, en;q=0.5'})
 
-    # The webpage URL
     URL = "https://www.amazon.com/s?k=playstation+4&ref=nb_sb_noss_2"
 
-    # HTTP Request
+
     webpage = requests.get(URL, headers=HEADERS)
 
-    # Soup Object containing all data
     soup = BeautifulSoup(webpage.content, "html.parser")
 
-    # Fetch links as List of Tag Objects
+
     links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})
 
-    # Store the links
     links_list = []
 
-    # Loop for extracting links from Tag Objects
     for link in links:
             links_list.append(link.get('href'))
 
     d = {"title":[], "price":[], "rating":[], "reviews":[],"availability":[]}
     
-    # Loop for extracting product details from each link 
     for link in links_list:
         new_webpage = requests.get("https://www.amazon.com" + link, headers=HEADERS)
 
         new_soup = BeautifulSoup(new_webpage.content, "html.parser")
 
-        # Function calls to display all necessary product information
         d['title'].append(get_title(new_soup))
         d['price'].append(get_price(new_soup))
         d['rating'].append(get_rating(new_soup))
